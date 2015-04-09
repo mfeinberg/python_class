bee7d1c0e405a01d38e10aa9dd1a868f65abb827
mfeinberg <mfeinberg88@gmail.com>
2015-04-08 21:01:13 -0400

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,10 @@
+sentence = "Thrift is poetic because it is creative; waste is unpoetic because it is waste."
+
+#email suggestion
+
+#google's advice
+words2 = sentence.split()
+words2_set = set(words2)
+len(words2_set)
+
+#attempt using for loop 
