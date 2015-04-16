4d7a1968e420137d404d9fc5d4e8b9b2a5f48cbf
mfeinberg <mfeinberg88@gmail.com>
2015-04-15 23:09:42 -0400

    Update printcsv.py

--- printcsv.py
+++ printcsv.py
@@ -1,55 +1,28 @@
-#3) CSV Format
+#Assignment 2: 3) CSV Format
 
 f = open('datatoformat2.csv','r') 
 lines = f.readlines() #reads lines in file and stores each line as an object in the list lines
 f.close()
 
-x = 0
-
-while x < len(lines):
+for x in range (0,len(lines)):
 	format = str(lines[x])
-	#format = format.replace(",","\t")
-	#print format
 	format_list = format.split(',')
-	#print format_list
-	y = 0
-	z = 0
-
 	if x == 0:
-		num_fields = len(format_list)
+		num_fields = len(format_list) #number of fields in the csv file
 		field_list = []
-		#num = 0
 		for num in range (0, num_fields):
 			field_list.insert(num, len(str(format_list[num])))
-		#print field_list
-		#while num < num_fields:
-		#	field_list.insert(num,len(str(format_list[num])))
-		#	print field_list
-		#	num += 1
-		#	print num
-		max_length = max(field_list)
-		#a = len(str(format_list[0]))
-		#b = len(str(format_list[1]))
-		#c = len(str(format_list[2]))
-		#d = len(str(format_list[3]))
-		#max_length = max(a,b,c,d)
-	while y < len(format_list):
-		if y != len(format_list)-1: 
-			#while len(str(format_list[y])) < max_length:
-				#format_list[y] = str(format_list[y]) + " "
-			format_list[y] = str(format_list[y]).ljust(max_length)
+		max_length = max(field_list) #finds field/column header with the longest length
+	for y in range (0, len(format_list)):
+		format_list[y] = str(format_list[y]).ljust(max_length) #left justifies each item in the list, padding with spaces as needed so each entry has the same length
 		if x == 0 and y != len(format_list)-1:
-			format_list[y] = str(format_list[y]) + ':'
-			formated_string = str(format_list[y])
-			formated_string = formated_string.replace(':','  |  ')
+			format_list[y] = str(format_list[y]) + '  |  ' #adds | to neatly separate field/column headers
+			formated_string = str(format_list[y])   
 			print formated_string,
 		elif y != len(format_list)-1:
-			format_list[y] = str(format_list[y]) + ':'
+			format_list[y] = str(format_list[y]) + '     ' #adds a few spaces for formatting purposes
 			formated_string = str(format_list[y])
-			formated_string = formated_string.replace(':','     ')
 			print formated_string,
 		else:
 			formated_string = str(format_list[y])
 			print formated_string
-		y+=1
-	x+=1
