1293c2d1ad49f54161409b8648075d12dfe20187
mfeinberg <mfeinberg88@gmail.com>
2015-04-16 20:43:57 -0400

    Update assignment2_print_class.py

--- assignment2_print_class.py
+++ assignment2_print_class.py
@@ -1,6 +1,14 @@
-f = open('file.csv')
+f = open('mock_data.csv')
 content = f.readlines()
 f.close()
-heard = content[0]
+header = content[0]
 data = content[1:]
- 
+width = 15
+
+#print out header in a nice way
+header_titles = header.split(",")
+print '| '.join(title.ljust(width).upper for title in header_titles)
+
+for line in data:
+	line_values = line.split(',')
+	print '  '.join(value.ljust(width) for value in line_values)
