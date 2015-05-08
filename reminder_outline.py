c5b8c7181afb55bc1fdce2121e8e34ca5ad571be
mfeinberg <mfeinberg88@gmail.com>
2015-05-07 20:57:36 -0400

    Update reminder_outline.py

--- reminder_outline.py
+++ reminder_outline.py
@@ -19,39 +19,62 @@ NEXT WEEK:
 '''
 
 # imports
-import sqlite3
+import datetime
 import sys
+import sqlite3
 
 # set up database connection
-connection = sqlite3.connect #connection to DB
+connection = sqlite3.connect('db.db') #connection to DB
 cursor = connection.cursor() #lets us manipulate DB
 
 # write our schema statement, e.g. CREATE TABLE reminders (id INTEGER PRIMARY KEY)
-#task_description text, datetime text, expired boolean, location text, notes text
-sql_schema = 'CREATE TABLE IF NOT EXISTS reminders(taskid INTEGER PRIMARY KEY, task TEXT, date TEXT, location TEXT, notes TEXT, expired BOOLEAN)'
+# task_description text, date text, expired boolean, location text, notes text
+sql_schema = '''CREATE TABLE IF NOT EXISTS reminders(task_id INTEGER PRIMARY KEY,
+task TEXT, date TEXT, location TEXT, notes TEXT, expired BOOLEAN)'''
 # note, integer primary key fields are autoincremented by default
-# create db table if not already existing
-#use the cursor to create the new table
+# use the cursor to create the new table
 cursor.execute(sql_schema)
 
+
 def getNewEvent():
-  prompt = 'Press [ENTER] to add a task of q/Q to quit '
-  response = raw_input(prompt).strip()
-  if responese in ['q','Q','quit','Quit','QUIT','EXIT','Exit','exit']:
-   sys.exit() #a way of quitting the program
-  else:
-   task = raw_input('Task: ')
-   year = raw_input('Year (yyyy): ')
-   month = raw_input('Month (mm): ')
-   day = raw_input('Day (dd): ')
-   hour = raw_input('Hour (hh): ')
-   minute = raw_input('Minute (mm): ')
-   am_or_pm = raw_input('AM or PM: ')
-   notes = raw_input('Additional information: ')
-   
-   data = [task,year,month,day,hour,minute,notes]
-   
-   cursor.execute('INSERT INTO reminders (task)
+	prompt = 'Press [ENTER] to add a task or q/Q to quit'
+	response = raw_input(prompt).strip()
+	if response in ['q','Q']:
+		sys.exit()
+
+	else:
+		data = [] #empty list that will be filled w/ user info and then inserted into DB
+		user_info = ['task: ','year (YYYY): ','month (MM) : ','day (DD): ','minute (MM): ','am or pm: ','location: ','additional information: ']
+		for question in user_info:
+			data.append(raw_input(question).strip())
+		time_date = [int(item) for item in data[1:5]]
+		time_string = datetime.datetime(time_data)
+		#task = raw_input('task: ').strip()
+		#year = raw_input('year (YYYY): ').strip()
+		#month = raw_input('month (MM) : ').strip()
+		#day = raw_input('day (DD): ').strip()
+		#hour = raw_input('hour (HH): ').strip()
+		#minute = raw_input('minute (MM): ').strip()
+		#am_or_pm = raw_input('am or pm: ').strip()
+		#location = raw_input('location: ').strip()
+		#notes = raw_input('additional information: ').strip()
+		expired = time_string < datetime.datetime.now()
+				
+		# use datetime.datetime to create a timestring object
+		#time_string = datetime.datetime(int(year),int(month),int(day),int(hour),int(minute))
+		
+		#data = (task,time_string,location,notes,expired)
+		cursor.execute('INSERT INTO reminders (task, date, location, notes, expired) \
+		VALUES (?,?,?,?,?)', data)
+		# commit this with connection.commit(), then fetch to make sure it worked
+		connection.commit()
+		
+		rows = cursor.execute('SELECT * FROM reminders')
+		#row == record
+		for row in rows:
+			for field in row:
+				print field
+			print '####' #breaks up records/neatly separates the different records
 
 def commitNewEvent(event):
   pass
@@ -62,7 +85,8 @@ def getNMostRecent(N=4):
 def getUpcomingEvents(N=4):
   pass
 
+
 def mainLoop():
-  pass
+  getNewEvent()
 
 mainLoop()
